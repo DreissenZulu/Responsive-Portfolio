622527a2987c679af39ccce68667d5824982daef
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-10-22 00:46:01 -0400

    Created objects for portfolio items

---
+++ assets/scripts/projects.js
@@ -0,0 +1,20 @@
+var projectsArr = [
+    {
+        title: "Password Generator",
+        desc: "Generates a password based on user-defined parameters. Users can copy the generated password to their clipboard",
+        pageUrl: "https://dreissenzulu.github.io/Password-Gen/",
+        hubUrl: "https://github.com/DreissenZulu/Password-Gen"
+    },
+    {
+        title: "JS Quiz Game",
+        desc: "A multiple choice quiz game about (mostly) JavaScript. Includes a locally saved scoreboard saved to localStorage",
+        pageUrl: "https://dreissenzulu.github.io/JS-Quiz-Game/",
+        hubUrl: "https://github.com/DreissenZulu/JS-Quiz-Game"
+    },
+    {
+        title: "Weather Description",
+        desc: "A weather dashboard which displays current weather and forecast for a searched city. Users can search for a city, and if found the app will return the current weather, as well as a forecast of the next 5 days. The user's search history is saved as a button for them to quickly check the weather of that city again.",
+        pageUrl: "https://dreissenzulu.github.io/weather-dashboard/",
+        hubUrl: "https://github.com/DreissenZulu/weather-dashboard"
+    }
+]
