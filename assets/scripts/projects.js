d9c4dd58cdfe3992681a097c43c1cfd31298ed07
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-10-22 10:44:36 -0400

    Changed title of Weather Dashboard

--- assets/scripts/projects.js
+++ assets/scripts/projects.js
@@ -12,7 +12,7 @@ var projectsArr = {
         hubUrl: "https://github.com/DreissenZulu/JS-Quiz-Game"
     },
     weathDash: {
-        title: "Weather Description",
+        title: "Weather Dashboard",
         desc: "A weather dashboard which displays current weather and forecast for a searched city. Users can search for a city, and if found the app will return the current weather, as well as a forecast of the next 5 days. The user's search history is saved as a button for them to quickly check the weather of that city again.",
         pageUrl: "https://dreissenzulu.github.io/weather-dashboard/",
         hubUrl: "https://github.com/DreissenZulu/weather-dashboard"
