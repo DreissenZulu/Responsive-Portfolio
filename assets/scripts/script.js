fb778a8d07eb5605bd6dcf3cd821432d4f234f93
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-10-22 01:03:04 -0400

    Attach pop up to buttons and allow pop up to close when clicked outside

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,22 @@
-$(".nav-item").click( function() {
+$(".nav-item").click(function () {
     $(".nav-item").removeClass("active")
     $(".container.main").addClass("d-none")
     $(this).addClass("active");
-    if (this.id == "about"){
+    if (this.id == "about") {
         $("#profileDiv").removeClass("d-none");
     } else if (this.id == "portfolio") {
         $("#portfolioDiv").removeClass("d-none");
     } else if (this.id == "contact") {
         $("#contactDiv").removeClass("d-none");
     }
+})
+
+$("#overlayPortfolio").click(function () {
+    if (event.target == event.currentTarget) {
+        $("#overlayPortfolio").addClass("d-none")
+    }
+})
+
+$("button").click(function() {
+    $("#overlayPortfolio").removeClass("d-none")
 })
