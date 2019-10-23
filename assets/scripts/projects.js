d9e6ba7520c8470f198322b3945e62bb9ca64ca3
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-10-23 11:06:17 -0400

    Changed object names to strings

--- assets/scripts/projects.js
+++ assets/scripts/projects.js
@@ -1,23 +1,23 @@
 var projectsArr = {
-    pwGen: {
+    "pwGen": {
         title: "Password Generator",
         desc: "Generates a password based on user-defined parameters. The user can choose to limit the password to any combination of lower case characters, upper case characters, numbers, and symbols, and set the length between 8 and 128 characters. Users can copy the generated password to their clipboard",
         pageUrl: "https://dreissenzulu.github.io/Password-Gen/",
         hubUrl: "https://github.com/DreissenZulu/Password-Gen"
     },
-    jsQuiz: {
+    "jsQuiz": {
         title: "JS Quiz Game",
         desc: "A multiple choice quiz game about (mostly) JavaScript. The application is designed to operate on a single page, except for the high scores page. Divs are dynamically hidden and shown using animation classes, and score is calculated based on time remaining and correct answers. Includes a scoreboard saved to localStorage",
         pageUrl: "https://dreissenzulu.github.io/JS-Quiz-Game/",
         hubUrl: "https://github.com/DreissenZulu/JS-Quiz-Game"
     },
-    weathDash: {
+    "weathDash": {
         title: "Weather Dashboard",
         desc: "A weather dashboard which displays current weather and forecast for a searched city. Users can search for a city, and if found the app will return the current weather, as well as a forecast of the next 5 days. The user's search history is saved as a button for them to quickly check the weather of that city again.",
         pageUrl: "https://dreissenzulu.github.io/weather-dashboard/",
         hubUrl: "https://github.com/DreissenZulu/weather-dashboard"
     },
-    pokeSim: {
+    "pokeSim": {
         title: "Pokemon Battle Sim",
         desc: "A project developed in a team of 4. Utilizes the server-side PokeAPI to develop a simple Pokemon battle simulator. Both teams have a team of six generated from the National Pokedex and battle until one side runs out of usable Pokemon. All information, including stats, sprites, and names, are pulled directly from the API when the page opens. Primary role: Lead Javascript programmer.",
         pageUrl: "https://bcbutcher.github.io/teamcapb/",
